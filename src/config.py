4fee65ab614363f30d0e846d1d31542c8307d36c
Morgan-Sell <morganpsell@gmail.com>
2024-11-03 10:54:03 -0500

    built postgres connection fcn

---
+++ src/config.py
@@ -0,0 +1,5 @@
+# Postgres Connection Parameters
+DB_NAME = "todo_list"
+DB_USER = "postgres"
+DB_HOST = "localhost"
+DB_PORT = "5432"
---
+++ src/storage.py
@@ -0,0 +1,21 @@
+import os
+
+import psycopg2
+from dotenv import load_dotenv
+from psycopg2 import sql
+
+from src.config import DB_HOST, DB_NAME, DB_PORT, DB_USER
+
+load_dotenv()
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+
+
+def connect_postgres_server():
+    conn = psycopg2.connect(
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT
+    )
